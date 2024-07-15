28a58bb6b9257c658eb60ee55c09ca4313a573f9
VedeshP <138141742+VedeshP@users.noreply.github.com>
2024-07-15 18:09:36 +0530

    Create password.js

---
+++ password.js
@@ -0,0 +1,10 @@
+function generatePassword(length = 12) {
+    const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return password;
+}
+
+console.log(generatePassword());
